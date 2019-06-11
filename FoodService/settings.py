c554ad564760fdd2adf0ce9f332cd43a81410bc5
Niayesh <niayesh.sadeghi@gmail.com>
2019-06-11 22:29:30 +0430

    [EDIT] minor changes

--- FoodService/settings.py
+++ FoodService/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'silk',
     'kitchen',
     'restaurant_admin',
     'customer',
@@ -54,7 +53,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'silk.middleware.SilkyMiddleware',
 ]
 
 ROOT_URLCONF = 'FoodService.urls'
