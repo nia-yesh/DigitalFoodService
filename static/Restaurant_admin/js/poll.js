25c3347325ff6234cd007b4fa0b09238480d19d8
sahand76 <sahandabaspout76@gmail.com>
2019-06-12 13:46:39 +0430

    home page

--- static/Restaurant_admin/js/poll.js
+++ static/Restaurant_admin/js/poll.js
@@ -1,18 +1,6 @@
-window.onscroll = function() {myFunction()};
 
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
- var text;
+var text;
 function myFunction(event){
-text=event.target.dataset.foodc;
-    document.getElementById("sahand").href="FoodCategory_delete/"+text;
-
+text=event.target.dataset.poll;
+    document.getElementById("sahand").href="Poll_delete/"+text;
 }
--- static/customer/js/category_detail.js
+++ static/customer/js/category_detail.js
@@ -1,50 +1,4 @@
-// // When the user scrolls the page, execute myFunction
-// window.onscroll = function() {myFunction2(),myFunction3()};
-//
-// // Get the header
-// var header = document.getElementById("myHeader");
-// var navi=document.getElementById("myTopnav");
-// // Get the offset position of the navbar
-// var sticky = header.offsetTop;
-// var sticky2=navi.offsetTop;
-//
-// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction2() {
-// if (window.pageYOffset > sticky) {
-// header.classList.add("sticky");
-// } else {
-// header.classList.remove("sticky");
-// }
-// }
-// function myFunction3() {
-// if (window.pageYOffset > sticky2) {
-// navi.classList.add("sticky2");
-// } else {
-// navi.classList.remove("sticky2");
-// }
-// }
-// /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-// function myFunction() {
-//   var x = document.getElementById("myTopnav");
-//   if (x.className === "topnav") {
-//     x.className += " responsive";
-//   }
-//   else if (x.className === "topnav sticky2") {
-//     x.className+= " responsive";
-//   }
-//    else {
-//     x.className = "topnav sticky2";
-//   }
-// }
-//
-// function myFunction4() {
-//   var x = document.getElementsByClassName("topnav sticky2");
-//   if (x.className === "topnav sticky2") {
-//     x.className += " responsive";
-//   } else {
-//     x.className = "topnav sticky2";
-//   }
-// }
+
 var buttons = document.getElementById('plus');
 
 Array.prototype.forEach.call(buttons, function (b) {
@@ -70,3 +24,14 @@ circle.classList.add('ripple');
 		circle.remove();
 	},500);
 }
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+     document.getElementById("uniq").setAttribute("class","btn btn-success btn-rounded btn-sm")
+    } else {
+        document.getElementById("uniq").setAttribute("class","btn btn-success btn-rounded btn-bg")
+    }
+  }
+  
+  var x = window.matchMedia("(max-width: 600px)")
+  myFunction(x) // Call listener function at run time
+  x.addListener(myFunction) // Attach listener function on state changes
